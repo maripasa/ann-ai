edd2dda97444185e3907622de00de9d85091c470
maripasa <asafexi@proton.me>
2025-10-08 18:53:54 -0300

    Made tree search
    for now it returns only a solution state, instead of a path

--- ia/src/MyLib.hs
+++ ia/src/MyLib.hs
@@ -1,4 +1,44 @@
-module MyLib (someFunc) where
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Search where
+
+-- Generic Problem
+class Action a
+class State s
+class (Action a, State s) => Problem p s a | p -> s a where
+  successor :: p -> a -> s -> s
+  isGoal :: p -> s -> Bool
+  actions :: p -> s -> [a]
+  startState :: p -> s
+  expand :: p -> s -> [s]
+
+-- Graph Problem
+data Graph a = Graph
+  { edges :: [(Node a, [Node a])], -- Node ID -> list of neighbor IDs
+    start :: Node a,
+    goal :: Node a
+  }
+
+newtype Node a = Node a
+
+newtype GraphAction a = GraphAction a
+
+instance Action (GraphAction a)
+
+instance State (Node a)
+
+instance Problem (Graph a) (Node a) (GraphAction a)
+
+-- Generic Search
+treeSearch :: (Problem p s a) => p -> ([s] -> (s, [s])) -> Maybe s
+treeSearch problem strategy = search [startState problem]
+  where
+    search [] = Nothing
+    search fringe
+      | isGoal problem node = Just node
+      | otherwise   = search (fringe' ++ expand problem node)
+      where (node, fringe') = strategy fringe
+
+graphSearch = id
+

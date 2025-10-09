d4bc05a4b6820fba658f6dac305454c6e584646b
maripasa <asafexi@proton.me>
2025-10-09 17:23:35 -0300

    ia: bfs, dfs, ucs, gs, a*

--- ia/src/Search.hs
+++ ia/src/Search.hs
@@ -1,11 +1,12 @@
 module Search where
 
 import Data.Foldable
+import Data.List
+import Data.Ord
 import qualified Data.Set as Set
-
 -- | Generic Problem
-data Problem s a = Problem
-  { successor :: s -> a -> (s, Int),
+data Problem s a c = Problem
+  { successor :: s -> a -> (s, c),
     isGoal :: s -> Bool,
     initial :: s,
     actions :: s -> [a]
@@ -16,7 +17,7 @@ newtype Strategy s a = Strategy ([(s, [a], Int)] -> ((s, [a], Int), [(s, [a], In
 
 -- | Generic Tree Search
 treeSearch ::
-  Problem s a -> 
+  Problem s a c-> 
   Strategy s a -> 
   Maybe [a]
 treeSearch problem (Strategy strategy) =
@@ -36,7 +37,7 @@ treeSearch problem (Strategy strategy) =
 -- | Generic Graph Search
 graphSearch ::
   (Ord s) =>
-  Problem s a -> -- Problem
+  Problem s a c -> -- Problem
   Strategy s a -> -- Strategy
   Maybe [a] -- Solution
 graphSearch problem (Strategy strategy) =
@@ -65,14 +66,40 @@ graphSearch problem (Strategy strategy) =
           , let (s', stepCost) = successor problem' node act
           ]
 
+-- Uninformed Search
+depthFirstSearch :: Strategy s a
+depthFirstSearch = Strategy (\fringe -> (last fringe, init fringe))
+
+breadthFirstSearch :: Strategy s a
+breadthFirstSearch = Strategy bfs
+  where
+    bfs [] = error "Breadth First Search can't use an empty fringe"
+    bfs (state:fs) = (state, fs)
+
+uniformCostSearch :: Strategy s a
+uniformCostSearch = Strategy $ \fringe ->
+  let sorted = sortBy (comparing (\(_,_,cost) -> cost)) fringe
+  in (head sorted, tail sorted)
+
+-- Informed Search
+greedySearch :: (Ord a) => (s -> a) -> Strategy s a
+greedySearch h = Strategy $ \fringe ->
+  let sorted = sortBy (comparing (\(state,_,_) -> h state)) fringe
+  in (head sorted, tail sorted)
+
+aStar :: (Num b, Ord b) => (s -> b) -> Strategy s a
+aStar h = Strategy $ \fringe ->
+  let sorted = sortBy (comparing (\(state, _, cost) -> cost + h state)) fringe
+  in (head sorted, tail sorted)
+
 -- | Graph Problem
-data Graph a = Graph
-  { edges :: [(a, [(a, Int)])], -- Node ID -> list of neighbor IDs
+data Graph a c = Graph
+  { edges :: [(a, [(a, c)])], -- Node ID -> list of neighbor IDs
     start :: a,
     goal :: a
   }
 
-graphProblem :: (Eq a) => Graph a -> Problem a a
+graphProblem :: (Eq a) => Graph a c -> Problem a a c
 graphProblem graph =
   Problem
     { successor = successor' graph,
@@ -86,4 +113,4 @@ graphProblem graph =
     findByNode space node =
       case find (\(n, _) -> n == node) space of
         Just a -> a
-        Nothing -> error "Node not present in the graph"
+        Nothing -> error "findByNode -> Node being choosed is not present in the graph"

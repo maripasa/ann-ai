eb3213ab750ed96999a38270ce80d90c07514a57
maripasa <asafexi@proton.me>
2025-10-09 16:14:48 -0300

    ia: Created graph search and data Strategy

--- ia/src/Search.hs
+++ ia/src/Search.hs
@@ -1,8 +1,9 @@
 module Search where
 
 import Data.Foldable
+import qualified Data.Set as Set
 
--- Generic Problem
+-- | Generic Problem
 data Problem s a = Problem
   { successor :: s -> a -> (s, Int),
     isGoal :: s -> Bool,
@@ -10,7 +11,61 @@ data Problem s a = Problem
     actions :: s -> [a]
   }
 
--- Graph Problem
+-- | Search Strategy
+newtype Strategy s a = Strategy ([(s, [a], Int)] -> ((s, [a], Int), [(s, [a], Int)]))
+
+-- | Generic Tree Search
+treeSearch ::
+  Problem s a -> 
+  Strategy s a -> 
+  Maybe [a]
+treeSearch problem (Strategy strategy) =
+  search [(initial problem, [], 0)] problem strategy
+  where
+    search [] _ _ = Nothing
+    search fringe problem' strategy'
+      | isGoal problem' node = Just path
+      | otherwise = search (fringe' ++ expandedNodes) problem' strategy'
+      where
+        ((node, path, cost), fringe') = strategy' fringe
+        expandedNodes =
+          [ (s', path ++ [act], cost + stepCost) | act <- actions problem' node,
+            let (s', stepCost) = successor problem' node act
+          ]
+
+-- | Generic Graph Search
+graphSearch ::
+  (Ord s) =>
+  Problem s a -> -- Problem
+  Strategy s a -> -- Strategy
+  Maybe [a] -- Solution
+graphSearch problem (Strategy strategy) =
+  search Set.empty [(initial problem, [], 0)] problem strategy
+  where
+    search ::
+      (Ord s) =>
+      Set.Set s -> -- Closed
+      [(s, [a], Int)] -> -- Fringe
+      Problem s a ->
+      ([(s, [a], Int)] -> ((s, [a], Int), [(s, [a], Int)])) ->
+      Maybe [a]
+    search _ [] _ _ = Nothing
+    search closed fringe problem' strategy'
+      | isGoal problem' node   = Just path
+      | Set.member node closed = search closed fringe' problem' strategy'
+      | otherwise =
+          search (Set.insert node closed)
+                 (fringe' ++ expandedNodes)
+                 problem'
+                 strategy'
+      where
+        ((node, path, cost), fringe') = strategy' fringe
+        expandedNodes =
+          [ (s', path ++ [act], cost + stepCost) | act <- actions problem' node
+          , let (s', stepCost) = successor problem' node act
+          ]
+
+-- | Graph Problem
 data Graph a = Graph
   { edges :: [(a, [(a, Int)])], -- Node ID -> list of neighbor IDs
     start :: a,
@@ -26,26 +81,9 @@ graphProblem graph =
       actions = map fst . snd . findByNode (edges graph)
     }
   where
-    successor' graph' node target = (`findByNode` target) . snd . findByNode (edges graph') $ node
-    findByNode space node = case find (\(n, _) -> n == node) space of
-      Just a -> a
-      Nothing -> error "Node not present in the graph"
-
--- Generic Search
-treeSearch ::
-  Problem s a ->
-  ([(s, [a], Int)] -> ((s, [a], Int), [(s, [a], Int)])) ->
-  Maybe [a]
-treeSearch problem = search [(initial problem, [], 0)] problem
-  where
-    search [] _ _ = Nothing
-    search fringe problem' strategy
-      | isGoal problem' node = Just path
-      | otherwise = search (fringe' ++ expandedNodes) problem' strategy
-      where
-        ((node, path, cost), fringe') = strategy fringe
-        expandedNodes =
-          [ (s', path ++ [act], cost + stepCost) | act <- actions problem' node,
-              let (s', stepCost) = successor problem' node act
-          ]
-
+    successor' graph' node target =
+      (`findByNode` target) . snd . findByNode (edges graph') $ node
+    findByNode space node =
+      case find (\(n, _) -> n == node) space of
+        Just a -> a
+        Nothing -> error "Node not present in the graph"
